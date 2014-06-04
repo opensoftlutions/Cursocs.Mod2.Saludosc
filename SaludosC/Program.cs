7ffce106ebcd1e8e724d88c1c28a34bc25c95f06
opensoftlutions <luisgil@opensoftlutions.com>
2014-06-04 12:39:49 +0200

    Curso de c#
    Proyecto de ejemplo

---
+++ SaludosC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludosC
+{
+    class Saludos
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es mi primer programa con C#");
+            
+            Console.ReadLine();
+        }
+    }
+}
